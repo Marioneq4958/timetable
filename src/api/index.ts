86780b6ba0c0966894d7866810290f5359d428b5
Marioneq <mq4958@gmail.com>
2025-07-12 19:42:56 +0200

    Add error when api origin var doesn't exist or is empty

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+const apiOrigin = import.meta.env.VITE_API_ORIGIN;
+if (!apiOrigin) throw new Error('Missing "VITE_API_ORIGIN" environment variable')
+
 export default axios.create({
   baseURL: import.meta.env.VITE_API_ORIGIN,
   headers: { 'Content-Encoding': 'application/json' },
