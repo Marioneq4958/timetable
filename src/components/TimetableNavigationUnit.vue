ccc1c0ff3aa1e0abb6365b411925868edb9a87e9
Marioneq <mq4958@gmail.com>
2025-07-12 18:18:08 +0200

    Add closing nav/drawer after picking an unit.

--- src/components/TimetableNavigationUnit.vue
+++ src/components/TimetableNavigationUnit.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
+import { useTimetableStore } from '@/stores/timetable.store';
 import type { RouteLocationAsRelativeGeneric } from 'vue-router';
 
 const props = defineProps<{ name: string; to: RouteLocationAsRelativeGeneric }>();
+const timetableStore = useTimetableStore();
 </script>
 
 <template>
   <li>
     <RouterLink
       :to="props.to"
+      @click="timetableStore.toggleMenu(false)"
       class="block px-3 py-2 rounded-md hover:bg-input/50 transition-all my-0.5 lg:text-sm"
       active-class="!bg-input/50"
     >
