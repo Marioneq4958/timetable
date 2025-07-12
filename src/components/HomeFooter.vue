64ead200ae1a26029a600a28c32dc34074eb2582
Marioneq <mq4958@gmail.com>
2025-07-12 17:03:31 +0200

    Correct home footer

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -1,21 +1,11 @@
-<script setup lang="ts">
-const getCurrentYear = () => new Date().getFullYear();
-</script>
-
 <template>
   <footer class="font-medium text-muted-foreground text-center text-xs">
-    &copy; {{ getCurrentYear() }} <br />
+    2025 <br />
     Stworzone przez
-    <a href="https://github.com/dominik-korsa" target="_blank" class="hover:underline">
-      Dominik Korsa
-    </a>
+    <a href="https://github.com/dominik-korsa" class="hover:underline">Dominik Korsa</a>
     i
-    <a href="https://github.com/Marioneq4958" target="_blank" class="hover:underline">
-      Marioneq4958 </a
-    >.
+    <a href="https://github.com/Marioneq4958" class="hover:underline">Marioneq4958</a>.
     <br />
-    <a href="https://github.com/Marioneq4958/timetable2.0" target="_blank" class="underline">
-      GitHub (MIT)
-    </a>
+    <a href="https://github.com/Marioneq4958/timetable" class="underline">GitHub</a>
   </footer>
 </template>
